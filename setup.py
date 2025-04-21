79adad26a439ca35477cf1d25f524a0c71710985
hitherelantern <lantern153pdt@gmail.com>
2025-04-21 10:23:28 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="lantern",
+    author_email="lantern153pdt@gmail.com",
+    packages=find_packages()
+)
